ec6a74030195bf83607f8a63241f2ca38f123d31
f2280c <f2280c@gmail.com>
2017-07-14 16:51:17 -0400

    initial commit

---
+++ font.js
@@ -0,0 +1,31 @@
+class font{
+  constructor(){
+    this.email_content = $(".email-content");
+  }
+  bold(){
+    this.email_content.append("<b>text</b>");
+  }
+  italicize(){
+    this.email_content.append("<i>text</i>");
+  }
+  unbold(){
+    this.email_content.append("text");
+  }
+  unitalicize(){
+    this.email_content.append("text");
+  }
+  boldSelection(){
+    var selection = window.getSelection().getRangeAt(0);
+    var content = selection.extractContents();
+    var b = document.createElement("b");
+    b.appendChild(content);
+    selection.insertNode(b);
+  }
+  italicizeSelection(){    
+    var selection = window.getSelection().getRangeAt(0);
+    var content = selection.extractContents();
+    var i = document.createElement("i");
+    i.appendChild(content);
+    selection.insertNode(i);
+  }
+}
