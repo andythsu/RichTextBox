f2cb46d5da5d00aa5192287cb44aeda80bc05d62
f2280c <f2280c@gmail.com>
2017-07-17 16:05:15 -0400

    unbold/ unitalicize selection

--- font.js
+++ font.js
@@ -15,19 +15,72 @@ class font{
     this.email_content.append("text");
   }
   boldSelection(){
-    var selection = window.getSelection().getRangeAt(0);
-    var content = selection.extractContents();
+    var range = window.getSelection().getRangeAt(0);
+    var content = range.extractContents();
     var b = document.createElement("b");
     b.appendChild(content);
-    selection.insertNode(b);
+    range.insertNode(b);
   }
+
+  unboldSelection(){
+    var selection = window.getSelection();
+    var range = selection.getRangeAt(0);
+    console.log("range is: " , range);
+    var whole_text = range.startContainer.textContent;
+    var start = range.startOffset;
+    var end = range.endOffset;
+    if (start == 0) {
+      var before_str = "";
+      var current_str = whole_text.substring(start, end);
+      var remaining_str = whole_text.substring(end);
+    }else{
+      var before_str = whole_text.substring(0, start);
+      var current_str = whole_text.substring(start, end);
+      var remaining_str = whole_text.substring(end);
+    }
+    before_str += "</b>";
+    current_str += "<b>";
+    remaining_str+="</b>";
+
+    var new_whole_text = before_str + current_str + remaining_str;
+
+    this.email_content.html(this.email_content.html().replace(whole_text, new_whole_text));
+  }
+
   italicizeSelection(){
-    var selection = window.getSelection().getRangeAt(0);
-    var content = selection.extractContents();
+    var range = window.getSelection().getRangeAt(0);
+    var content = range.extractContents();
     var i = document.createElement("i");
     i.appendChild(content);
-    selection.insertNode(i);
+    range.insertNode(i);
   }
+
+  unitalicizeSelection(){
+    var selection = window.getSelection();
+    var range = selection.getRangeAt(0);
+    console.log("range is: " , range);
+    var whole_text = range.startContainer.textContent;
+    var start = range.startOffset;
+    var end = range.endOffset;
+    if (start == 0) {
+      var before_str = "";
+      var current_str = whole_text.substring(start, end);
+      var remaining_str = whole_text.substring(end);
+    }else{
+      var before_str = whole_text.substring(0, start);
+      var current_str = whole_text.substring(start, end);
+      var remaining_str = whole_text.substring(end);
+    }
+    before_str += "</i>";
+    current_str += "<i>";
+    remaining_str+="</i>";
+
+    var new_whole_text = before_str + current_str + remaining_str;
+
+    this.email_content.html(this.email_content.html().replace(whole_text, new_whole_text));
+
+  }
+
   href(name, link){
     var a = document.createElement("a");
     a.href = link;
